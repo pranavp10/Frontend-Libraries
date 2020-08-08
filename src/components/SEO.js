e92f636cfb3423c63bba45df949382377aa8b84e
Pranav <pranavkp.me@outlook.com>
2020-08-08 20:49:24 +0530

    seo fix

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -3,13 +3,13 @@ import { NextSeo } from 'next-seo';
 const SEO = () => (
   <NextSeo
     title="Frontend Libraries"
-    description="Best Frontend Libraries which has been used by folks working at facebook,Google,Microsoft and may more."
+    description=" Best Frontend Libraries which are used by professionals working at facebook, Google, Microsoft and many other top companies."
     canonical="https://libraries.now.sh/"
     openGraph={{
       url: 'https://libraries.now.sh/',
       title: 'Frontend Libraries',
       description:
-        'Best Frontend Libraries which has been used by folks working at facebook,Google,Microsoft and may more.',
+        ' Best Frontend Libraries which are used by professionals working at facebook, Google, Microsoft and many other top companies.',
       images: [
         {
           url: 'https://libraries.now.sh/shareSEO.png',
