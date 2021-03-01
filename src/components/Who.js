b28b4e414a7e885d00052f954d38b88934aa665d
Pranav Patel <pranav@quze.co>
2021-03-01 17:01:35 +0530

    :mag: SEO and performance improved

--- src/components/Who.js
+++ src/components/Who.js
@@ -5,11 +5,11 @@ const Who = () => (
   <div>
     <div className="flex max-w-5xl m-auto items-center flex-col p-6 sm:flex-row  justify-center lg:justify-between my-48">
       <div>
-        <h5 className="mb-4 text-6xl font-extrabold">
+        <h4 className="mb-4 text-6xl font-extrabold">
           New npm
           <br />
           packages ?
-        </h5>
+        </h4>
         <div className="max-w-md m-auto text-xl my-7">
           I am always starving to try out new npm packages so that I can implement those in my up
           coming projects. Do you know any other packages that I could try out? Hit me up, let's
@@ -32,12 +32,12 @@ const Who = () => (
           name="Pranav Patel"
           src="/author.jpeg"
         />
-        <h6 className="text-5xl font-extrabold">Pranav Patel</h6>
+        <h5 className="text-5xl font-extrabold">Pranav Patel</h5>
         <p className="text-xl text-gray-500 dark:text-gray-400 my-2">Software Engineer</p>
-        <p className="max-w-xs m-auto text-lg">
+        <h6 className="max-w-xs m-auto text-lg">
           JavaScript, React, Design Systems, a software engineer by passion, loves solving problems.
           Most loved thing in js is the closure property, figuring out life's purpose.
-        </p>
+        </h6>
         <div className="text-center flex justify-center space-x-2 mt-5">
           <a
             className="text-blue-500 hover:text-blue-700"
